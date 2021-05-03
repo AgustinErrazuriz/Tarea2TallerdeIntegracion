81a21a4a2f98a10e285edcc76b2bccfbadd4be79
AgustinErrazuriz <aierrazuriz@uc.cl>
2021-05-03 00:16:38 -0400

    7

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,7 @@ class Artists(APIView):
         serializer = ArtistasSerializer(artista1, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request, id):
         datos=request.data
         if type(datos['name']) is not str:
             return Response(status=status.HTTP_400_BAD_REQUEST)
