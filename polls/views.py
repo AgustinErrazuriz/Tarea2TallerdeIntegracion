c3ae235c32579100cb916afa111591756c310283
AgustinErrazuriz <aierrazuriz@uc.cl>
2021-05-03 01:54:45 -0400

    25

--- polls/views.py
+++ polls/views.py
@@ -225,8 +225,8 @@ class Track_id(APIView):
         if len(track) == 0:
             return Response(status=status.HTTP_404_NOT_FOUND)
         cancion = Canciones.objects.get(id=id)
-        serializer = CancionesSerializer(cancion, many=True)
-        return Response(serializer.data[0], status=status.HTTP_200_OK)
+        serializer = CancionesSerializer(cancion)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, id):
         track = Canciones.objects.filter(id=id)
